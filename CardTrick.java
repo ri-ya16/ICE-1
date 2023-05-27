3b877bb8aeac982e32b403082ad3dca351697c76
ri-ya16 <133277082+ri-ya16@users.noreply.github.com>
2023-05-27 18:14:43 -0400

    Add files via upload

---
+++ CardTrick.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package card;
+
+/**
+ * A class that fills a magic hand of 7 cards with random Card Objects
+ * and then asks the user to pick a card and searches the array of cards
+ * for the match to the user's card. To be used as starting code in ICE 1
+ * @author srinivsi
+ */
+import java.util.Random;
+import java.util.Scanner;
+public class CardTrick {
+    
+    public static void main(String[] args)
+    {
+        Card[] magicHand = new Card[7];
+        
+        for (int i=0; i<magicHand.length; i++)
+        {
+            Card c = new Card();
+            //c.setValue(insert call to random number generator here)
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(random.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[random.nextInt(4)]);
+            magicHand[i] = c;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Pick a card value (1-13): ");
+        int value = scanner.nextInt();
+        scanner.nextLine(); // Consume newline character
+
+        System.out.print("Pick a card suit (Hearts, Diamonds, Clubs, Spades): ");
+        String suit = scanner.nextLine();
+
+        Card userCard = new Card();
+        userCard.setValue(value);
+        userCard.setSuit(suit);
+
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == userCard.getValue() && card.getSuit().equalsIgnoreCase(userCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Congratulations! Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
+        
+        //insert code to ask the user for Card value and suit, create their card
+        // and search magicHand here
+        //Then report the result here
+    }
+    
+}
